857764be3a889cca79a3aecf73b6bb411e88193a
Dylan Rutter <dylanrutter@rrcs-67-78-97-3.sw.biz.rr.com>
2018-12-09 16:24:43 -0600

    add some comments

--- src/part2.js
+++ src/part2.js
@@ -1,9 +1,9 @@
 function homeawayLoop(range, multiples, words) {
   
     for (let i = range[0]; i <= range[1]; i++) {
-      if (i % multiples[0] === 0 && i % multiples[1] === 0) console.log(words[0] + words[1]);
-      else if (i % multiples[0] === 0) console.log(words[0]);
-      else if (i % multiples[1] === 0) console.log(words[1]);
+      if (i % multiples[0] === 0 && i % multiples[1] === 0) console.log(words[0] + words[1]); //this makes it print both words if remainder is 0
+      else if (i % multiples[0] === 0) console.log(words[0]); //prints first first if first option remainder is 0
+      else if (i % multiples[1] === 0) console.log(words[1]); //same as above but for 2nd option 
       else console.log(i);
     }     
   } 
